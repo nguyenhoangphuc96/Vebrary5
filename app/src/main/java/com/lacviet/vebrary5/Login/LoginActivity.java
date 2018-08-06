e4dfe4d03860d93d65312f0b49511d3b5fc9fae8
nguyenhoangphuc96 <nguyenhoangphuc171196@gmail.com>
2018-08-06 11:17:04 +0700

    init project and layout login

---
+++ app/src/main/java/com/lacviet/vebrary5/Login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.lacviet.vebrary5.Login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lacviet.vebrary5.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
