91f73b5b9957b6713ed5ba563ef5a52d049cec11
nguyenhoangphuc96 <nguyenhoangphuc171196@gmail.com>
2018-08-06 17:37:46 +0700

    complete layout login

--- app/src/main/java/com/lacviet/vebrary5/Login/LoginActivity.java
+++ app/src/main/java/com/lacviet/vebrary5/Login/LoginActivity.java
@@ -2,14 +2,27 @@ package com.lacviet.vebrary5.Login;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 import com.lacviet.vebrary5.R;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText edtId,edtPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        addControl();
+
+        addEvent();
+    }
+
+    private void addEvent() {
+    }
+
+    private void addControl() {
+        edtId = findViewById(R.id.edtID);
+        edtPass = findViewById(R.id.edtPassword);
     }
 }
